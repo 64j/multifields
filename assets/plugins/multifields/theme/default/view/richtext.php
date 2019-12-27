5df34390e50a7fcf1fa2467fc14ec2b4633bc1b5
Serg <64j@mail.ru>
2019-12-27 13:32:45 +0300

    fix edit icon

--- assets/plugins/multifields/theme/default/view/richtext.php
+++ assets/plugins/multifields/theme/default/view/richtext.php
@@ -1,4 +1,6 @@
 <div class="mf-col mf-richtext input-group<?= $class ?>" data-type="<?= $type ?>" data-name="<?= $name ?>">
     <?= $element ?>
-    <i class="fa fa-pencil btn" onclick="MultiFields.getRichText(this.previousElementSibling, '<?= $tvId ?>', '<?= $elements ?>')"></i>
+    <div class="btn" onclick="MultiFields.getRichText(this.previousElementSibling, '<?= $tvId ?>', '<?= $elements ?>')">
+        <i class="fa fa-pencil"></i>
+    </div>
 </div>
