19800720dffdd178fe3cf63a54629b4bd0f04865
Serg <64j@mail.ru>
2020-04-18 22:32:26 +0300

    add lang element Table

---
+++ assets/plugins/multifields/elements/table/lang/english.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'add_column' => 'Add column',
+    'del_column' => 'Delete column',
+    'type.text' => 'Text',
+    'type.number' => 'Number',
+    'type.date' => 'Date',
+    'type.image' => 'Image',
+    'type.file' => 'File',
+];
---
+++ assets/plugins/multifields/elements/table/lang/russian-UTF8.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'add_column' => 'Добавить колонку',
+    'del_column' => 'Удалить колонку',
+    'type.text' => 'Текст',
+    'type.number' => 'Число',
+    'type.date' => 'Дата',
+    'type.image' => 'Изображение',
+    'type.file' => 'Файл',
+];
