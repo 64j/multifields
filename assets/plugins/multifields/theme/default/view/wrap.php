9d51c1414a21ddbd21257c8cf514ca6a747a9699
Kamil <milkamil93@gmail.com>
2020-05-22 14:38:11 +0500

    Fix for ClientSettings, set storage default

--- assets/plugins/multifields/theme/default/view/wrap.php
+++ assets/plugins/multifields/theme/default/view/wrap.php
@@ -2,4 +2,4 @@
     <?= $toolbar ?>
     <?= $items ?>
 </div>
-<textarea name="<?= $fieldname ?>" data-tvid="<?= $tvId ?>" data-tvname="<?= $tvName ?>" data-multifields rows="10"><?= $value ?></textarea>
+<textarea name="<?= (!is_int($tvId) ? 'tv' : '') . $fieldname ?>" data-tvid="<?= $tvId ?>" data-tvname="<?= $tvName ?>" data-multifields rows="10"><?= $value ?></textarea>
