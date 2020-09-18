4523e6879b3f391fa31856e2663e922969f8b514
Serg <64j@mail.ru>
2020-09-18 12:05:42 +0300

    added config for toolbar

---
+++ assets/plugins/multifields/config/example.toolbar.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'settings' => [
+        //'view' => 'icons',
+
+        //'breakpoints' => true, // default breakpoints will be set ['', 'xl', 'lg', 'md', 'sm', 'xs']
+        'breakpoints' => ['', 'xl', 'lg', 'sm'],
+        'export' => true,
+        'import' => true,
+        'fullscreen' => true,
+    ],
+
+    'templates' => [
+        // ...
+        'row' => [
+            'type' => 'row'
+        ]
+    ]
+];
